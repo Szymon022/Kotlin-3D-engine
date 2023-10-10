324c01976485b8f155f953e4d60a16fd66f76af9
Szymon022 <szymon.kurpas00@gmail.com>
2023-10-10 17:09:05 +0200

    Add ViewModel

---
+++ src/main/kotlin/ViewModel.kt
@@ -0,0 +1,16 @@
+import androidx.compose.ui.graphics.ImageBitmap
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+class ViewModel {
+
+    private val _scene = MutableStateFlow(initialScene())
+    val scene = _scene.asStateFlow()
+
+    private fun initialScene() = ImageBitmap(WIDTH, HEIGHT)
+
+    companion object {
+        const val WIDTH = 900
+        const val HEIGHT = 600
+    }
+}
