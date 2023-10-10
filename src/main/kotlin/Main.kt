2d47135fb03e1ab8eab1851536c1311c41b93cb0
Szymon022 <szymon.kurpas00@gmail.com>
2023-10-10 17:16:53 +0200

    Draw scene window

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,31 +1,45 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.material.Button
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowState
 import androidx.compose.ui.window.application
 
 @Composable
 @Preview
-fun App() {
-    var text by remember { mutableStateOf("Hello, World!") }
-
-    MaterialTheme {
-        Button(onClick = {
-            text = "Hello, Desktop!"
-        }) {
-            Text(text)
-        }
+fun Scene(bitmap: ImageBitmap, modifier: Modifier = Modifier) {
+    Canvas(modifier = modifier) {
+        drawImage(bitmap)
     }
 }
 
 fun main() = application {
-    Window(onCloseRequest = ::exitApplication) {
-        App()
+    Window(
+        state = WindowState(
+            size = DpSize(
+                (ViewModel.WIDTH + 200).dp,
+                ViewModel.HEIGHT.dp + 40.dp
+            ),
+        ),
+        onCloseRequest = ::exitApplication
+
+    ) {
+        MaterialTheme {
+            val viewModel = remember { ViewModel() }
+            val scene by viewModel.scene.collectAsState()
+            Scene(
+                modifier = Modifier.size(width = ViewModel.WIDTH.dp, height = ViewModel.HEIGHT.dp),
+                bitmap = scene
+            )
+        }
     }
 }
