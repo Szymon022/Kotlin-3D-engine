1efe7c0b6e60258f2a44d75a9ec45bca145509ec
Szymon022 <szymon.kurpas00@gmail.com>
2023-10-10 15:12:56 +0200

    Fix typo in parseObj that caused bug

--- src/main/kotlin/obj/ObjParser.kt
+++ src/main/kotlin/obj/ObjParser.kt
@@ -11,9 +11,9 @@ fun parseObj(path: String): Model {
     val faces = mutableListOf<Face>()
     File(path).forEachLine { line ->
         when {
-            line.startsWith("v") -> parseVec3(line, header = "v").also(vertices::add)
-            line.startsWith("vn") -> parseVec3(line, header = "vn").also(vertices::add)
-            line.startsWith("f") -> {
+            line.startsWith("v ") -> parseFloat3(line, header = "v").also(vertices::add)
+            line.startsWith("vn ") -> parseFloat3(line, header = "vn").also(normals::add)
+            line.startsWith("f ") -> {
                 line.substringAfter("f ")
                     .split(" ")
                     .also { iPairs ->
@@ -28,12 +28,12 @@ fun parseObj(path: String): Model {
     return Model(faces.toTypedArray())
 }
 
-fun parseVec3(string: String, header: String, delimiter: String = " ") = string.substringAfter("$header$delimiter")
+fun parseFloat3(string: String, header: String, delimiter: String = " ") = string.substringAfter("$header$delimiter")
     .split(delimiter)
     .map(String::toFloat)
-    .toVec3()
+    .toFloat3()
 
-private fun List<Float>.toVec3() = Float3(
+private fun List<Float>.toFloat3() = Float3(
     x = this[0],
     y = this[1],
     z = this[2],
