85c8219176bb02aa9455bc1911cb66de993ae946
Szymon022 <szymon.kurpas00@gmail.com>
2023-10-12 11:08:03 +0200

    Make drawing filledModel concurrent

--- src/main/kotlin/draw/DrawFilledModel.kt
+++ src/main/kotlin/draw/DrawFilledModel.kt
@@ -4,24 +4,28 @@ import androidx.compose.ui.graphics.Canvas
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.Paint
+import data.Face
 import data.Model
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.*
 import kotlin.system.measureTimeMillis
 
+@OptIn(ExperimentalCoroutinesApi::class)
 fun drawFilledModel(width: Int, height: Int, model: Model): Flow<ImageBitmap> = flow {
     measureTimeMillis {
         val image = ImageBitmap(width = width, height = height)
-
         val paint = Paint().apply { color = Color.Blue }
         val scale = height / 2
         Canvas(image).apply {
-            model.array.forEach {
-                drawFilledPolygon(face = it.translateAndScale(scale), paint = paint)
-            }
+            model.array.asFlow().flatMapMerge(concurrency = 4) {
+                drawFilledPolygonFlow(this, it, scale, paint)
+            }.flowOn(Dispatchers.Default).collect()
         }
         emit(image)
     }.also { println("rendered in $it ms") }
 }.flowOn(Dispatchers.Default)
+
+fun drawFilledPolygonFlow(canvas: Canvas, face: Face, scale: Int, paint: Paint) = flow<Unit> {
+    canvas.drawFilledPolygon(face.translateAndScale(scale), paint = paint)
+}
