80ffac4b036b154e3f4d9bd34e6ac314620de0cd
Szymon022 <szymon.kurpas00@gmail.com>
2023-10-12 12:14:13 +0200

    Implement BufferedImage equivalence of drawingFilledPolygon

--- src/main/kotlin/draw/DrawFilledModel.kt
+++ src/main/kotlin/draw/DrawFilledModel.kt
@@ -1,31 +1,31 @@
 package draw
 
-import androidx.compose.ui.graphics.Canvas
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.ImageBitmap
-import androidx.compose.ui.graphics.Paint
+import androidx.compose.ui.graphics.*
 import data.Face
 import data.Model
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.*
+import java.awt.image.BufferedImage
 import kotlin.system.measureTimeMillis
 
 @OptIn(ExperimentalCoroutinesApi::class)
 fun drawFilledModel(width: Int, height: Int, model: Model, color: Color): Flow<ImageBitmap> = flow {
     measureTimeMillis {
-        val image = ImageBitmap(width = width, height = height)
+        val bufferedImage = BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB)
         val paint = Paint().apply { this.color = color }
         val scale = height / 2
-        Canvas(image).apply {
-            model.array.asFlow().flatMapMerge(concurrency = 4) {
-                drawFilledPolygonFlow(this, it, scale, paint)
-            }.flowOn(Dispatchers.Default).collect()
-        }
-        emit(image)
+        model.array.asFlow().flatMapMerge(concurrency = 8) {
+            drawFilledPolygonFlow(bufferedImage, it, scale, paint)
+        }.flowOn(Dispatchers.Default).collect()
+        emit(bufferedImage.toComposeImageBitmap())
     }.also { println("rendered in $it ms") }
 }.flowOn(Dispatchers.Default)
 
 fun drawFilledPolygonFlow(canvas: Canvas, face: Face, scale: Int, paint: Paint) = flow<Unit> {
     canvas.drawFilledPolygon(face.translateAndScale(scale), paint = paint)
+}
+
+fun drawFilledPolygonFlow(bitmap: BufferedImage, face: Face, scale: Int, paint: Paint) = flow<Unit> {
+    bitmap.drawFilledPolygon(face.translateAndScale(scale), color = paint.color.toArgb())
 }
--- src/main/kotlin/draw/DrawFilledPolygon.kt
+++ src/main/kotlin/draw/DrawFilledPolygon.kt
@@ -6,6 +6,7 @@ import androidx.compose.ui.graphics.Paint
 import data.Edge
 import data.Face
 import data.toEdgeTable
+import java.awt.image.BufferedImage
 
 fun Canvas.drawFilledPolygon(face: Face, paint: Paint) {
     val edgeTable = face.toEdgeTable().also { it.sortBy(Edge::yMin) }
@@ -15,6 +16,20 @@ fun Canvas.drawFilledPolygon(face: Face, paint: Paint) {
     while (!(edgeTable.isEmpty() && activeEdgeTable.isEmpty())) {
         edgeTable.moveToAET(activeEdgeTable, scanline)
         drawLine(activeEdgeTable, scanline, paint)
+        scanline++
+        activeEdgeTable.removeIf { it.yMax < scanline }
+        activeEdgeTable.updateEdges()
+    }
+}
+
+fun BufferedImage.drawFilledPolygon(face: Face, color: Int) {
+    val edgeTable = face.toEdgeTable().also { it.sortBy(Edge::yMin) }
+    val activeEdgeTable = mutableListOf<Edge>()
+    var scanline = edgeTable.first().yMin
+
+    while (!(edgeTable.isEmpty() && activeEdgeTable.isEmpty())) {
+        edgeTable.moveToAET(activeEdgeTable, scanline)
+        drawLine(activeEdgeTable, scanline, color)
         scanline++
         activeEdgeTable.removeIf { it.yMax < scanline }
         activeEdgeTable.updateEdges()
@@ -44,6 +59,24 @@ fun Canvas.drawLine(aet: MutableList<Edge>, scanline: Float, paint: Paint) {
             p2 = Offset(x = e2.x, y = scanline),
             paint = paint
         )
+    }
+}
+
+fun BufferedImage.drawLine(aet: MutableList<Edge>, scanline: Float, color: Int) {
+    for (i in 0 until aet.size - 1 step 2) {
+        val e1 = aet[i]
+        val e2 = aet[i + 1]
+        drawLine(x1 = e1.x, x2 = e2.x, y = scanline, color = color)
+    }
+}
+
+fun BufferedImage.drawLine(x1: Float, x2: Float, y: Float, color: Int) {
+    var x = x1.toInt()
+    val y = y.toInt()
+    val x2 = x2.toInt()
+    while (x <= x2) {
+        setRGB(x, y, color)
+        x++
     }
 }
 
