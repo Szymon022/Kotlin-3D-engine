3b5a7b0ebcb4fae34d43e771a9129ad99d89d15a
Szymon022 <szymon.kurpas00@gmail.com>
2023-10-13 13:33:39 +0200

    Test Float3 length

--- src/test/kotlin/data/Float3Test.kt
+++ src/test/kotlin/data/Float3Test.kt
@@ -1,4 +1,27 @@
 package data
 
-class Float3 {
-}
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.floats.plusOrMinus
+import io.kotest.matchers.shouldBe
+import kotlin.math.sqrt
+
+class Float3Test : FunSpec({
+
+    context("Length") {
+
+        test("Returns correct length if 0 vector is given") {
+            val v = Float3(0f, 0f, 0f)
+            v.length.shouldBe(0f plusOrMinus 1e-7f)
+        }
+
+        test("Returns correct length for 3d vector") {
+            val v = Float3(1f, 1f, 1f)
+            v.length.shouldBe(sqrt(3f) plusOrMinus 1e-7f)
+        }
+
+        test("Returns correct length") {
+            val v = Float3(3f, 4f, 5f)
+            v.length.shouldBe(sqrt(50f) plusOrMinus 1e-7f)
+        }
+    }
+})
