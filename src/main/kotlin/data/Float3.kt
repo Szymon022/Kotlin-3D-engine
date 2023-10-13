9f85ec4d7227beb81800c7f99e9c80d2e929550a
Szymon022 <szymon.kurpas00@gmail.com>
2023-10-13 13:32:59 +0200

    Add length property to Float3

--- src/main/kotlin/data/Float3.kt
+++ src/main/kotlin/data/Float3.kt
@@ -1,7 +1,12 @@
 package data
 
+import kotlin.math.sqrt
+
 data class Float3(
     val x: Float,
     val y: Float,
     val z: Float
-)
+) {
+
+    val length: Float get() = sqrt(x * x + y * y + z * z)
+}
