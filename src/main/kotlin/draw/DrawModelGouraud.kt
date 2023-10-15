a0abf4e7ea10cac57c8d42e2e9eff89efec4ce28
Szymon022 <szymon.kurpas00@gmail.com>
2023-10-15 09:03:07 +0200

    Add method for drawin method with Gouraud method

--- src/main/kotlin/draw/DrawModelGouraud.kt
+++ src/main/kotlin/draw/DrawModelGouraud.kt
@@ -1,2 +1,79 @@
 package draw
 
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.toComposeImageBitmap
+import data.Face
+import data.Float3
+import data.Model
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.*
+import light.drawTriangleGouraud
+import math.normalize
+import java.awt.image.BufferedImage
+import kotlin.system.measureTimeMillis
+
+@OptIn(ExperimentalCoroutinesApi::class)
+fun drawModelGouraud(width: Int, height: Int, model: Model, color: Color): Flow<ImageBitmap> = flow {
+    measureTimeMillis {
+        val bufferedImage = BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB)
+        val scale = height / 2
+        val lightColor = Color.White.let {
+            Float3(
+                x = it.red,
+                y = it.green,
+                z = it.blue
+            )
+        }
+        val objColor = Color.Red.let {
+            Float3(
+                x = it.red,
+                y = it.green,
+                z = it.blue
+            )
+        }
+        val observer = Float3(0f, 0f, 1f)
+        val light = Float3(1f, 1f, 0f).normalize()
+        model.array.asFlow().flatMapMerge(concurrency = 8) {
+            drawTriangleGouraudFlow(
+                bitmap = bufferedImage,
+                face = it,
+                scale = scale,
+                lightColor = lightColor,
+                objColor = objColor,
+                light = light,
+                observer = observer,
+            )
+
+        }.flowOn(Dispatchers.Default).collect()
+        emit(bufferedImage.toComposeImageBitmap())
+    }.also { println("rendered in $it ms") }
+}.flowOn(Dispatchers.Default)
+
+
+fun drawTriangleGouraudFlow(
+    bitmap: BufferedImage,
+    face: Face,
+    scale: Int,
+    lightColor: Float3,
+    objColor: Float3,
+    light: Float3,
+    observer: Float3,
+    kd: Float = 1f,
+    ks: Float = 1f,
+    m: Int = 10,
+) = flow<Unit> {
+
+    bitmap.drawTriangleGouraud(
+        face = face.translateAndScale(scale),
+        kd = kd,
+        ks = ks,
+        m = m,
+        lightColor = lightColor,
+        objColor = objColor,
+        light = light,
+        observer = observer,
+    )
+}
+
