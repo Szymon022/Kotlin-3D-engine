f3b54c7b3831a140a60275eacd8d1eec9c632de8
Szymon022 <szymon.kurpas00@gmail.com>
2023-10-22 21:02:11 +0200

    Change isNotVisible to isVisible

--- src/main/kotlin/culling/BackFaceCulling.kt
+++ src/main/kotlin/culling/BackFaceCulling.kt
@@ -6,8 +6,8 @@ import math.minus
 import math.o
 import math.x
 
-fun Face.isNotVisible(camera: Float3): Boolean {
+fun Face.isVisible(camera: Float3): Boolean {
     val (v1, v2, v3) = this.vertices
     val normal = (v2 - v1) x (v3 - v1)
-    return normal o camera >= 0f
+    return normal o camera < 0f
 }
