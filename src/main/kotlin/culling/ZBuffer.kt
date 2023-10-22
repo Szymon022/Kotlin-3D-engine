fdefd77ee00061064c3565ba9b2c8c52bcac3b76
Szymon022 <szymon.kurpas00@gmail.com>
2023-10-22 21:38:22 +0200

    Add mutex

--- src/main/kotlin/culling/ZBuffer.kt
+++ src/main/kotlin/culling/ZBuffer.kt
@@ -1,6 +1,10 @@
 package culling
 
+import kotlinx.coroutines.sync.Mutex
+
 class ZBuffer(val width: Int, val height: Int) {
+
+    val mutex = Mutex()
 
     private val buffer = FloatArray(width * height) { Float.MAX_VALUE }
 
