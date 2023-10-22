3de6e55883bf61b718aec7270e955302a535b954
Szymon022 <szymon.kurpas00@gmail.com>
2023-10-22 22:51:28 +0200

    Add rotation matrices for x, y, z rotations

--- src/main/kotlin/data/Matrix4x4.kt
+++ src/main/kotlin/data/Matrix4x4.kt
@@ -1,5 +1,8 @@
 package data
 
+import kotlin.math.cos
+import kotlin.math.sin
+
 @JvmInline
 value class Matrix4x4 private constructor(private val data: Array<FloatArray>) {
 
@@ -11,4 +14,40 @@ value class Matrix4x4 private constructor(private val data: Array<FloatArray>) {
     ) : this(arrayOf(row1, row2, row3, row4))
 
     operator fun get(i: Int, j: Int) = data[i][j]
+}
+
+fun xRotationMatrix(degrees: Float): Matrix4x4 {
+    val rad = Math.toRadians(degrees.toDouble()).toFloat()
+    val cos = cos(rad)
+    val sin = sin(rad)
+    return Matrix4x4(
+        floatArrayOf(1f, 0f, 0f, 0f),
+        floatArrayOf(0f, cos, -sin, 0f),
+        floatArrayOf(0f, sin, cos, 0f),
+        floatArrayOf(0f, 0f, 0f, 1f),
+    )
+}
+
+fun yRotationMatrix(degrees: Float): Matrix4x4 {
+    val rad = Math.toRadians(degrees.toDouble()).toFloat()
+    val cos = cos(rad)
+    val sin = sin(rad)
+    return Matrix4x4(
+        floatArrayOf(cos, 0f, sin, 0f),
+        floatArrayOf(0f, 1f, 0f, 0f),
+        floatArrayOf(-sin, 0f, cos, 0f),
+        floatArrayOf(0f, 0f, 0f, 1f),
+    )
+}
+
+fun zRotationMatrix(degrees: Float): Matrix4x4 {
+    val rad = Math.toRadians(degrees.toDouble()).toFloat()
+    val cos = cos(rad)
+    val sin = sin(rad)
+    return Matrix4x4(
+        floatArrayOf(cos, -sin, 0f, 0f),
+        floatArrayOf(sin, cos, 0f, 0f),
+        floatArrayOf(0f, 0f, 1f, 0f),
+        floatArrayOf(0f, 0f, 0f, 1f),
+    )
 }
