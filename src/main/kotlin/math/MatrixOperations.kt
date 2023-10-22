7ca047795a34e2ed4744677c4363ab4b5e1bf300
Szymon022 <szymon.kurpas00@gmail.com>
2023-10-22 22:49:47 +0200

    Add matrix multiplication

--- src/main/kotlin/math/MatrixOperations.kt
+++ src/main/kotlin/math/MatrixOperations.kt
@@ -13,3 +13,21 @@ operator fun Matrix4x4.times(v: Float4): Float4 {
         w = m[3, 0] * x + m[3, 1] * y + m[3, 2] * z + m[3, 3] * w,
     )
 }
+
+operator fun Matrix4x4.times(b: Matrix4x4): Matrix4x4 {
+    val a = this
+    val result = arrayOf(
+        floatArrayOf(0f, 0f, 0f, 0f),
+        floatArrayOf(0f, 0f, 0f, 0f),
+        floatArrayOf(0f, 0f, 0f, 0f),
+        floatArrayOf(0f, 0f, 0f, 0f),
+    )
+    for (i in 0 until 4) {
+        for (j in 0 until 4) {
+            for (k in 0 until 4) {
+                result[i][j] += a[i, k] * b[k, j]
+            }
+        }
+    }
+    return Matrix4x4(result[0], result[1], result[2], result[3])
+}
