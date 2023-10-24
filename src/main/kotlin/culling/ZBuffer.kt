816cd3ae8c6d4b3539bb153bfb2d62bb403d56c6
Szymon022 <szymon.kurpas00@gmail.com>
2023-10-24 16:58:31 +0200

    Fix get and set

--- src/main/kotlin/culling/ZBuffer.kt
+++ src/main/kotlin/culling/ZBuffer.kt
@@ -8,9 +8,9 @@ class ZBuffer(val width: Int, val height: Int) {
 
     private val buffer = FloatArray(width * height) { Float.MAX_VALUE }
 
-    operator fun get(x: Int, y: Int) = buffer[x * width + y]
+    operator fun get(x: Int, y: Int) = buffer[x + y * width]
 
     operator fun set(x: Int, y: Int, value: Float) {
-        buffer[x * width + y] = value
+        buffer[x + y * width] = value
     }
 }
