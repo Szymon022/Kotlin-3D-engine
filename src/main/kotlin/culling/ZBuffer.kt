4b5f485626607dce99104e1b28af2aa5e05b2086
Szymon022 <szymon.kurpas00@gmail.com>
2023-10-24 16:58:50 +0200

    Remove redundant mutext

--- src/main/kotlin/culling/ZBuffer.kt
+++ src/main/kotlin/culling/ZBuffer.kt
@@ -1,10 +1,6 @@
 package culling
 
-import kotlinx.coroutines.sync.Mutex
-
 class ZBuffer(val width: Int, val height: Int) {
-
-    val mutex = Mutex()
 
     private val buffer = FloatArray(width * height) { Float.MAX_VALUE }
 
