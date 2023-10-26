b21147a5ae60af29900e1c5eda13e4498436c040
Szymon022 <szymon.kurpas00@gmail.com>
2023-10-26 10:24:39 +0200

    Fix perspective matrix

--- src/main/kotlin/math/3DTransformations.kt
+++ src/main/kotlin/math/3DTransformations.kt
@@ -3,16 +3,23 @@ package math
 import androidx.compose.ui.graphics.Matrix
 import kotlin.math.tan
 
-fun Matrix.projection(width: Int, height: Int, fovAngle: Float): Matrix {
-    val cx = width / 2f
-    val cy = height / 2f
-    val s = cy / tan(fovAngle / 180f * 3.141592f)
+fun createPerspectiveFieldOfView(
+    fovAngle: Float,
+    aspectRatio: Float,
+    nearPlaneDistance: Float,
+    farPlaneDistance: Float
+): Matrix {
+    val fovRadians = fovAngle * 3.141592f / 180f
+    val cx = 1f / tan(fovRadians / 2f)
+    val cy = cx / aspectRatio
+    val (n, f) = nearPlaneDistance to farPlaneDistance
+    val common = f.toDouble() / (n.toDouble() - f.toDouble())
     return Matrix(
         floatArrayOf(
-            s, 0f, cx, 0f,
-            0f, s, cy, 0f,
-            0f, 0f, 0f, 1f,
-            0f, 0f, 1f, 0f,
+            cx, 0f, 0f, 0f,
+            0f, cy, 0f, 0f,
+            0f, 0f, common.toFloat(), -1f,
+            0f, 0f, (n.toDouble() * common).toFloat(), 0f,
         )
     )
 }
