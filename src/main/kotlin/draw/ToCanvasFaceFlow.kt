88f34ad3c5d3ee047b990c2db075b3fe81144350
Szymon022 <szymon.kurpas00@gmail.com>
2023-10-26 11:36:41 +0200

    Moved Float4.toCanvas to ToCanvasFaceFlow file

--- src/main/kotlin/draw/ToCanvasFaceFlow.kt
+++ src/main/kotlin/draw/ToCanvasFaceFlow.kt
@@ -4,8 +4,9 @@ import androidx.compose.ui.graphics.Matrix
 import data.CanvasFace
 import data.Face
 import data.Float3
+import data.Float4
 import kotlinx.coroutines.flow.flow
-import math.toCanvas
+import math.transform
 import math.transformNormal
 import java.awt.image.BufferedImage
 
@@ -29,5 +30,22 @@ fun toCanvasFaceFlow(face: Face, canvas: BufferedImage, model: Matrix, lookAt: M
     val transformedNormals = normals.map { it.transformNormal(model) }.toTypedArray()
     emit(CanvasFace(vertices = canvasVertices, normals = transformedNormals))
 }
+
+private fun Float4.toCanvas(model: Matrix, lookAt: Matrix, perspective: Matrix, width: Int, height: Int) =
+    transform(model)
+        .transform(lookAt)
+        .transform(perspective)
+        .normalize()
+        .toCanvas(
+            width = width,
+            height = height
+        )
+
+private fun Float3.toCanvas(width: Int, height: Int) = copy(
+    x = 0.5f * width * (1f + x / 1f),
+    y = 0.5f * height * (1f + y / 1f)
+)
+
+private fun Float4.normalize() = Float3(x / w, y / w, z / w)
 
 private inline fun Float3.isVisible(width: Int, height: Int) = x >= 0 && y >= 0 && x < width && y < height
