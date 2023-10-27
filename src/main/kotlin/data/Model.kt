85a4d5fc3998d2cc61f323448ebff554f09c5feb
Szymon022 <szymon.kurpas00@gmail.com>
2023-10-27 15:34:44 +0200

    Add color and modelMatrix to Model class

--- src/main/kotlin/data/Model.kt
+++ src/main/kotlin/data/Model.kt
@@ -1,4 +1,16 @@
 package data
 
-@JvmInline
-value class Model(val faces: Array<Face>)
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Matrix
+
+data class Model(
+    val faces: Array<Face>,
+    val color: Color,
+    val modelMatrix: Matrix = Matrix(),
+    private val onMatrixUpdate: (Float) -> Matrix = { modelMatrix },
+) {
+
+    fun update(time: Float) {
+        modelMatrix.setFrom(onMatrixUpdate(time))
+    }
+}
