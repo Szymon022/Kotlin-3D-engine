34636475391fba5a589c024f3a99a131d6afa620
Szymon022 <szymon.kurpas00@gmail.com>
2023-10-27 15:41:06 +0200

    Change Model into class and add secondaty constructor

--- src/main/kotlin/data/Model.kt
+++ src/main/kotlin/data/Model.kt
@@ -3,14 +3,21 @@ package data
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Matrix
 
-data class Model(
+class Model private constructor(
     val faces: Array<Face>,
     val color: Color,
-    val modelMatrix: Matrix = Matrix(),
-    private val onMatrixUpdate: (Float) -> Matrix = { modelMatrix },
+    val matrix: Matrix,
+    private val onMatrixUpdate: (Float) -> Matrix,
 ) {
 
+    constructor(faces: Array<Face>, color: Color, onMatrixUpdate: (Float) -> Matrix) : this(
+        faces = faces,
+        color = color,
+        onMatrixUpdate = onMatrixUpdate,
+        matrix = onMatrixUpdate(0f),
+    )
+
     fun update(time: Float) {
-        modelMatrix.setFrom(onMatrixUpdate(time))
+        matrix.setFrom(onMatrixUpdate(time))
     }
 }
