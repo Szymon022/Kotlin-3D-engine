7440c6e8b2567289e34deb0e5fcd47139579e71f
Szymon022 <szymon.kurpas00@gmail.com>
2023-11-03 16:49:53 +0100

    Fix not working fog

--- src/main/kotlin/fog/Fog.kt
+++ src/main/kotlin/fog/Fog.kt
@@ -17,16 +17,16 @@ fun BufferedImage.addFog(zBuffer: ZBuffer, fog: Fog) {
             val z = zBuffer[x, y]
             if (z > fog.minRange && z < fog.maxRange) {
                 val alpha = (z - fog.minRange) / (fog.maxRange - fog.minRange)
-                val rgb = Color(getRGB(x, y))
-                setRGB(x, y, blend(rgb, fog.color, alpha).toArgb())
+                val rgb = getRGB(x, y)
+                setRGB(x, y, blend(rgb, fog.color.toArgb(), alpha))
             }
         }
     }
 }
 
-private fun blend(color: Color, fogColor: Color, alpha: Float): Color {
-    val r = color.red * (1 - alpha) + fogColor.red * alpha
-    val g = color.green * (1 - alpha) + fogColor.green * alpha
-    val b = color.blue * (1 - alpha) + fogColor.blue * alpha
-    return Color(red = r, green = g, blue = b)
+private fun blend(color: Int, fogColor: Int, alpha: Float): Int {
+    val r = (((color and 0xFF0000) shr 16) * (1 - alpha) + ((fogColor and 0xFF0000) shr 16) * alpha).toInt() shl 16
+    val g = (((color and 0xFF00) shr 8) * (1 - alpha) + ((fogColor and 0xFF00) shr 8) * alpha).toInt() shl 8
+    val b = ((color and 0xFF) * (1 - alpha) + (fogColor and 0xFF) * alpha).toInt()
+    return (-16777216 or r or g or b)
 }
