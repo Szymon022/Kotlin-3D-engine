57a9c482de1ebbbe41388ea32bdd701cd6cf8880
Szymon022 <szymon.kurpas00@gmail.com>
2023-11-04 09:58:24 +0100

    Add sliders for min-max range of fog

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,7 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
-import androidx.compose.material.CircularProgressIndicator
-import androidx.compose.material.MaterialTheme
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -43,12 +40,52 @@ fun main() = application {
         MaterialTheme {
             val scene by viewModel.scene.collectAsState(viewModel.initialScene())
             val isLoadingResources by viewModel.isLoadingResources.collectAsState()
-            Box(modifier = Modifier.size(width = MainViewModel.WIDTH.dp, height = MainViewModel.HEIGHT.dp)) {
-                Scene(modifier = Modifier.fillMaxSize(), bitmap = scene)
-                if (isLoadingResources) {
-                    CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
+            Row(modifier = Modifier.fillMaxSize()) {
+                Box(modifier = Modifier.size(width = MainViewModel.WIDTH.dp, height = MainViewModel.HEIGHT.dp)) {
+                    Scene(modifier = Modifier.fillMaxSize(), bitmap = scene)
+                    if (isLoadingResources) {
+                        CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
+                    }
                 }
+                SidePanel(
+                    modifier = Modifier.fillMaxHeight().width(200.dp),
+                    viewModel = viewModel
+                )
             }
         }
+    }
+}
+
+@Composable
+fun SidePanel(viewModel: MainViewModel, modifier: Modifier = Modifier) {
+    val fog by viewModel.fog.collectAsState()
+    Column(modifier = modifier) {
+        LabeledGroup(label = "Fog controls") {
+            Text(text = "minRange: ${String.format("%.3f", fog.minRange)}")
+            Slider(
+                modifier = Modifier.fillMaxWidth(),
+                value = fog.minRange,
+                onValueChange = viewModel::onMinFogRangeChange,
+                valueRange = 0f..3f
+            )
+            Spacer(Modifier.height(4.dp))
+            Text(text = "maxRange: ${String.format("%.3f", fog.maxRange)}")
+            Slider(
+                modifier = Modifier.fillMaxWidth(),
+                value = fog.maxRange,
+                onValueChange = viewModel::onMaxFoxRangeChange,
+                valueRange = 0f..3f
+            )
+        }
+        Divider(modifier = Modifier.fillMaxWidth())
+    }
+}
+
+@Composable
+fun LabeledGroup(label: String, modifier: Modifier = Modifier, content: @Composable ColumnScope.() -> Unit = {}) {
+    Column(modifier = modifier) {
+        Text(text = label)
+        Box(modifier = Modifier.height(4.dp))
+        content()
     }
 }
