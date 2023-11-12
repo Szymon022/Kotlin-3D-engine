be0d78b3dd97762534096d7592eebf46cabbc99f
Szymon022 <szymon.kurpas00@gmail.com>
2023-11-12 08:30:48 +0100

    Smplify drawModelPhong method

--- src/main/kotlin/draw/DrawModelPhong.kt
+++ src/main/kotlin/draw/DrawModelPhong.kt
@@ -7,7 +7,6 @@ import androidx.compose.ui.graphics.toComposeImageBitmap
 import culling.ZBuffer
 import culling.isVisible
 import data.Camera
-import data.CanvasFace
 import data.Float3
 import data.Model
 import kotlinx.coroutines.Dispatchers
@@ -15,7 +14,6 @@ import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.*
 import light.drawTrianglePhong
 import java.awt.image.BufferedImage
-import kotlin.system.measureTimeMillis
 
 @OptIn(ExperimentalCoroutinesApi::class)
 fun drawModelPhong(
@@ -27,58 +25,28 @@ fun drawModelPhong(
     light: Float3,
     camera: Camera,
 ): Flow<ImageBitmap> = flow {
-    measureTimeMillis {
-        model.faces.asFlow().flatMapMerge { face ->
-            toCanvasFaceFlow(
-                canvas = bufferedImage,
-                face = face,
-                model = model.matrix,
-                lookAt = camera.lookAt,
-                perspective = camera.perspective,
+    model.faces.asFlow().flatMapMerge { face ->
+        toCanvasFaceFlow(
+            canvas = bufferedImage,
+            face = face,
+            model = model.matrix,
+            lookAt = camera.lookAt,
+            perspective = camera.perspective,
+        )
+    }.filter { it.isVisible(camera.target) }
+        .onEach { canvasFace ->
+            bufferedImage.drawTrianglePhong(
+                face = canvasFace,
+                zBuffer = zBuffer,
+                kd = shadingParams.kd,
+                ks = shadingParams.ks,
+                m = shadingParams.m,
+                lightColor = lightColor,
+                objColor = model.color,
+                light = light,
+                camera = camera,
             )
-        }.filter { it.isVisible(camera.target) }
-            .flatMapMerge(concurrency = 8) {
-                drawTrianglePhongFlow(
-                    bitmap = bufferedImage,
-                    zBuffer = zBuffer,
-                    face = it,
-                    lightColor = lightColor,
-                    objColor = model.color,
-                    light = light,
-                    camera = camera,
-                    kd = shadingParams.kd,
-                    ks = shadingParams.ks,
-                    m = shadingParams.m
-                )
-            }.flowOn(Dispatchers.Default).collect()
-        emit(bufferedImage.toComposeImageBitmap())
-    }.also { println("rendered in $it ms") }
+        }.flowOn(Dispatchers.Default)
+        .collect()
+    emit(bufferedImage.toComposeImageBitmap())
 }.flowOn(Dispatchers.Default)
-
-
-fun drawTrianglePhongFlow(
-    bitmap: BufferedImage,
-    zBuffer: ZBuffer,
-    face: CanvasFace,
-    lightColor: Color,
-    objColor: Color,
-    light: Float3,
-    camera: Camera,
-    kd: Float = 1f,
-    ks: Float = 1f,
-    m: Int = 10,
-) = flow {
-
-    bitmap.drawTrianglePhong(
-        face = face,
-        zBuffer = zBuffer,
-        kd = kd,
-        ks = ks,
-        m = m,
-        lightColor = lightColor,
-        objColor = objColor,
-        light = light,
-        camera = camera,
-    )
-    emit(Unit)
-}
